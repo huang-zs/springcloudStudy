eaaf6e92bd83347647c3ca687d225178a55a6fe2
huang-zs <2601377840@qq.com>
2020-07-07 16:16:14 +0800

    springcloud-bus

---
+++ springcloud-config-client-bus/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@RestController
+class HelloController {
+    @Value("${test.msg}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
